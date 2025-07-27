054e367153495bc49bb0004a4fd0fb4f1159f9d5
raworiginal <randall.a.wolfe@gmail.com>
2025-07-27 11:59:07 -0700

    up to date

---
+++ controllers/listings.js
@@ -0,0 +1,8 @@
+/* ===================== Dependencies ===================== */
+const express = require("express");
+const router = express.Router();
+
+const Listing = require("../models/listing");
+
+/* ===================== Dependencies ===================== */
+module.exports = router;
---
+++ models/listing.js
@@ -0,0 +1,33 @@
+/* ===================== Dependencies ===================== */
+const mongoose = require("mongoose");
+
+/* ===================== Models ===================== */
+const listingSchema = mongoose.Schema({
+  streetAddress: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  size: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+
+/* ===================== Exports ===================== */
+module.exports = Listing;
